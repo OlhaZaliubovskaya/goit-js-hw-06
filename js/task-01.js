8dfcdd7cb6d4b5cf8f1f6056bb6f144de525ef47
OlhaZaliubovskaya <110026359+OlhaZaliubovskaya@users.noreply.github.com>
2022-12-04 15:54:02 +0200

    д/з 6
    зробила 2 задачкі

---
+++ js/task-01.js
@@ -0,0 +1,9 @@
+const categories = document.getElementById('categories');
+console.log(`Number of categories: ${categories.children.length}`);
+for (const item of categories.children) {
+    const title = item.firstElementChild;
+    const elements = item.lastElementChild.children.length;
+    console.log(`Caterory: '${title.textContent}'`);
+    console.log(`Elements : ${elements}`);
+};
+
