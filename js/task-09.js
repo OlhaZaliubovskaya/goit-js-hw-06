8f24f55fe8021d780d505267127127840321b36c
OlhaZaliubovskaya <110026359+OlhaZaliubovskaya@users.noreply.github.com>
2022-12-06 23:29:54 +0200

    домашка goit-js-hw-06.

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,15 @@
+const changeBtn = document.querySelector(".change-color");
+const colorHexCode = document.querySelector(".color");
+
+changeBtn.addEventListener("click", changeColor);
+
+function changeColor(event) {
+    document.body.style.backgroundColor = getRandomHexColor();
+    colorHexCode.textContent = document.body.style.backgroundColor;
+};
+
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-}
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+};
