12270e882e9b342ae2040892bf3e4ba3f20505ee
OlhaZaliubovskaya <110026359+OlhaZaliubovskaya@users.noreply.github.com>
2022-12-06 11:11:40 +0200

    д/з 6
    виконала 5 завдання

---
+++ js/task-05.js
@@ -0,0 +1,12 @@
+const input = document.getElementById("name-input");
+const output = document.getElementById("name-output");
+
+input.addEventListener("input", outputOperation);
+
+function outputOperation(event) {
+    output.textContent = event.currentTarget.value;
+
+    if (!input.value) {
+        output.textContent="Anonymous"
+    };
+}
